2c6f7a216a1e78888c67776d453a34cbaf2c98aa
shaw <shaw@qq.com>
2022-10-01 15:26:21 +0800

    xxz new1

--- src/main/java/com/xxz/controller/EmpController.java
+++ src/main/java/com/xxz/controller/EmpController.java
@@ -5,6 +5,7 @@ import com.xxz.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,8 +23,9 @@ public class EmpController {
         return "index";
     }
 
+    //条件查询
     @RequestMapping("/emps")
-    public String queryAll(Model model){
+    public String queryAll(Model model, String rename, String eJob){
         System.out.println("queryAll emp ....");
         List<Employee> employeeList = employeeMapper.selectByExample(null);
         System.out.println(employeeList);
@@ -37,6 +39,27 @@ public class EmpController {
         System.out.println("add emp..." + employee);
         //调用接口将数据添加到数据库
         employeeMapper.insertSelective(employee);
+        return "redirect:/emp/emps";
+    }
+
+    //删除员工
+    @RequestMapping("/del/{eId}")
+    public String empDel(@PathVariable("eId") Integer eId){
+        System.out.println(eId);
+        //删除业务
+        employeeMapper.deleteByPrimaryKey(eId);
+        //重定向到empList界面展示最新数据
+        return "redirect:/emp/emps";
+    }
+
+    //修改
+    @RequestMapping("/update")
+    public String empUpdate(Employee employee){
+        //展示emp
+        System.out.println(employee);
+        //调用目标接口实现信息修改
+        employeeMapper.updateByPrimaryKeySelective(employee);
+        //重定向到empMenu界面
         return "redirect:/emp/emps";
     }
 
